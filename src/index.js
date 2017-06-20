7d127e8fee6fe3bc368f56187b26ef095fe2eb34
Lepoldo Caballero <git@pitakill.net>
2017-06-19 20:26:50 -0500

    JS without Flow

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const {log} = console;
+
+//////////
+
+const String = 'Hola Mundo!';
+const Number = 8;
+
+const receiveNumber = number => number;
+
+[Number, String].forEach(el => log(receiveNumber(el)));
+
+//////////
+
+const token = 666;
+const tokenString = '666';
+
+const validateNumber = number => {
+  if ('number' !== typeof number) {
+    return new Error('Number not valid');
+  }
+};
+
+const validateToken = token => {
+  try {
+    const error = validateNumber(token);
+    if (error) throw error;
+
+    log(`Tenemos un token válido: ${token}`);
+
+  }
+  catch (e) {
+    log(`Algo salió terriblemente mal: ${e}`);
+  }
+};
+
+[token, tokenString].forEach(el => validateToken(el));
